568376ff0e0c6be530250d66e9db161cc9903ede
Dmitry Boychevsky <javashdrug@yandex.ru>
2023-04-08 22:22:29 +0300

    Add java project

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,3 +1,14 @@
 public class BmiService {
+    public int calculate(double heightM, int weightKilo) {
+        double heightSquared;
+        heightSquared = heightM * heightM;
+
+        double bodyMassIndex = weightKilo / heightSquared;
+        //bodyMassIndex = weightKilo / heightSquared;
+
+        return (int) bodyMassIndex;
+
+    }
+
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
+        BmiService service = new BmiService();
+
+        int bodyMassIndex = service.calculate (1.77, 96);
+        System.out.println( " индекс массы тела " + bodyMassIndex);
+
 
     }
 }
